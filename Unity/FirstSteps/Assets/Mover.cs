eda69a809fbf835f7df741320cd2e1c3d4c1834e
Erdenay <erdenaycafer1153@gmail.com>
2026-04-17 03:31:56 +0300

    Add a ground plane, RigidBody physics, and camera follow

--- Unity/FirstSteps/Assets/Mover.cs
+++ Unity/FirstSteps/Assets/Mover.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     public float speed = 5f;
+    private Rigidbody rb;
 
-    void Update()
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
     {
         float moveX = Input.GetAxis("Horizontal"); // A/D or Left/Right arrows
         float moveZ = Input.GetAxis("Vertical");   // W/S or Up/Down arrows
 
-        Vector3 movement = new Vector3(moveX, moveZ, 0f);
-
-        transform.Translate(movement * speed * Time.deltaTime);
+        Vector3 movement = new Vector3(moveX, 0f, moveZ);
+        rb.AddForce(movement * speed);
     }
 }
